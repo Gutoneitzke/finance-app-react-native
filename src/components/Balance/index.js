497099fe2561b835982ab78fd5293633f998f880
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-12-30 18:44:09 -0300

    feat: new flatlist, new props to balance and new component to movements

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Balance() {
+export default function Balance({balance, expenses}) {
  return (
     <View style={styles.container}>
         <View style={styles.item}>
             <Text style={styles.itemTitle}>Saldo</Text>
             <View style={styles.content}>
                 <Text style={styles.currencySymbol}>R$</Text>
-                <Text style={styles.balance}>15.000,00</Text>
+                <Text style={styles.balance}>
+                    {balance}
+                </Text>
             </View>
         </View>
 
@@ -16,7 +18,9 @@ export default function Balance() {
             <Text style={styles.itemTitle}>Gastos</Text>
             <View style={styles.content}>
                 <Text style={styles.currencySymbol}>R$</Text>
-                <Text style={styles.expenses}>3.500,00</Text>
+                <Text style={styles.expenses}>
+                    {expenses}
+                </Text>
             </View>
         </View>
     </View>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header/index';
 import Balance from '../../components/Balance/index';
+import Movements from '../../components/Movements/index';
+
+const list = [
+  {
+    id: 1,
+    label: 'Boleto LUZ',
+    value: '300,10',
+    date: '20.12.2022',
+    type: 0 //gastos
+  },
+  {
+    id: 2,
+    label: 'Salário',
+    value: '500,10',
+    date: '13/10/2022',
+    type: 1 //extrada
+  },
+  {
+    id: 3,
+    label: 'Compra mercado',
+    value: '13,10',
+    date: '01/11/2022',
+    type: 0 //gastos
+  },
+]
 
 export default function Home() {
   return (
     <View style={styles.container}>
         <Header name="@gutoneitzke" />
-        <Balance />
-        
+        <Balance balance="9.250,00" expenses="250,00" />
+        <Text style={styles.title}>Últimas movimentações</Text>
+        <FlatList 
+          style={styles.list} 
+          data={list} 
+          keyExtractor={(item) => String(item.id)}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => <Movements /> }
+        ></FlatList>
     </View>
   );
 }
@@ -17,4 +49,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fafafa'
   },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 14
+  },
+  list:{
+    marginStart: 14,
+    marginEnd: 14
+  }
 });
